14655b6241cf636badab27835fd65a56dc09bfc7
jesus.changbuccheri <jesus.changbuccheri@PLX300000000664>
2020-04-16 15:50:31 +0200

    Agregado RepositoryDDBB con Persistencia siulada. Agregada carpeta de json con datos

---
+++ TestConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _3.Datos;
+
+namespace TestConsole {
+    class Program {
+        static void Main(string[] args) {
+            RepositoryDAL.CrearDDBB();
+            foreach (DTOTest t in RepositoryDAL.tests)
+            {
+                Console.WriteLine(t.id + t.nombre);
+            }
+            Console.ReadLine();
+        }
+    }
+}
