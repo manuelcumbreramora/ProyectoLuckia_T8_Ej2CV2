b1d429ab717bab3ecdaada8ada43385090e6e067
Roiben <62422631+RoibenGomezNion@users.noreply.github.com>
2020-04-16 15:36:56 +0200

    Add class and properties
    Added class DTOMonedero and properties to Monedero

---
+++ 3.Datos/DTOMonedero.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.Datos
+{
+    public class DTOMonedero
+    {
+        public float saldo { get; set; }
+        public int idMonedero { get; set; }
+        public string divisa { get; set; }
+        public int idUsuario { get; set; }
+        public DTOMonedero()
+        {
+
+        }
+    }
+}
