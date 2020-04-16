2c1e12b54d8e8ffdca6633386b97dea960b9a868
davidcrespopardo <62425693+davidcrespopardo@users.noreply.github.com>
2020-04-16 13:46:58 +0200

    transaccion and dtotransaccion classes added
    dadadasdasdasdasdasdasdasdasdasdasdasdasd

---
+++ 3.Datos/DTOTransaccion.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.Datos
+{
+    public class DTOTransaccion
+    {
+
+        public DTOTransaccion(int importe, int idMonedero, string tipoTransaccion, DateTime fechaCreacion, int idTransaccion)
+        {
+            this.importe = importe;
+            this.idMonedero = idMonedero;
+            this.tipoTransaccion = tipoTransaccion;
+            this.fechaCreacion = fechaCreacion;
+            this.idTransaccion = idTransaccion;
+        }
+
+        public int idTransaccion { get; set; }
+        public int importe { get; set; }
+        public int idMonedero { get; set; }
+        public DateTime fechaCreacion { get; set; }
+        public string tipoTransaccion { get; set; }
+
+
+
+
+
+    }
+}
