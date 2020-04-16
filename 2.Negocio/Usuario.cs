9e0a179a8f1d36a84a915d48540107b8972377e7
AlejandroTD257 <AlejandroTorres257@gmail.com>
2020-04-16 13:28:13 +0200

    Añadida la clase  Usuario a Negocio

---
+++ 2.Negocio/Usuario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Negocio
+{
+    public class Usuario
+    {
+        public string login { get; set; }
+        public string pass { get; set; }
+        public string estadoCuenta { get; set; }
+        public int idUsuario { get; set; }
+        public Monedero monederoUsuario { get; set; }
+        public DateTime fechaCreacion { get; set; }
+
+        public bool CrearUsuario(string login,string pass)
+        {
+            //Llamada a la BBDD
+            return false;
+        }
+    }
+}
